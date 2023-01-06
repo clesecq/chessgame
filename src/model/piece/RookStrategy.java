0083db2fec6283fafe79e88a0372af34eb4f3525
Charles LESECQ <charles.lesecq@etu.unistra.fr>
2023-01-06 18:12:51 +0100

    fix rook strategy

--- src/model/piece/RookStrategy.java
+++ src/model/piece/RookStrategy.java
@@ -16,15 +16,24 @@ public class RookStrategy implements PieceStrategy {
 
     @Override
     public int[][] getMovements(int x, int y) {
-        int[][] movements = new int[16][2];
+        int[][] movements = new int[18][2];
+        int index = 0;
 
-        for (int i = 0; i < 8; i++)
-            if (i != y) movements[i < y ? i : i - 1] = new int[]{x, i};
-        movements[7] = new int[]{-1, -1}; // new chain
+        for (int i = x+1; i < 8; i++)
+            movements[index++] = new int[]{i, y};
+        movements[index++] = new int[]{-1, -1};
 
-        for (int i = 0; i < 8; i++)
-            if (i != x) movements[8 + (i < x ? i : i - 1)] = new int[]{i, y};
-        movements[15] = new int[]{-1, -1}; // new chain
+        for (int i = x-1; i >= 0; i--)
+            movements[index++] = new int[]{i, y};
+        movements[index++] = new int[]{-1, -1};
+
+        for (int i = y+1; i < 8; i++)
+            movements[index++] = new int[]{x, i};
+        movements[index++] = new int[]{-1, -1};
+
+        for (int i = y-1; i >= 0; i--)
+            movements[index++] = new int[]{x, i};
+        movements[index++] = new int[]{-1, -1};
 
         return movements;
     }
